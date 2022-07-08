39772766c671bb9e552ae2fa1d356c2d0935b641
AnushaPanchumarthi <anooshap.rao@gmail.com>
2022-07-08 16:12:04 +0530

    Minor corrections

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import express from 'express';
 import routes from './routes/indexRoute';
 
 const app = express();
-const port = 3000;
+const port = 3000 as number;
 
-//stratin point for the route api
+//Initial point for the route api
 app.use('/api', routes);
-app.get('/', (req, res) => {
+app.get('/', function (req: express.Request, res: express.Response): void {
   res.send('Image Processor API');
 });
 
 //start the express server
-app.listen(port, () => {
+app.listen(port, function (): void {
   console.log(`server started at http://localhost:${port}`);
 });
 
